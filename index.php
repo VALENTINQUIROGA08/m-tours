f03bf342ab5c16c380efbf4b3c17487ba70fa668
VALENTINQUIROGA08 <VALENTINQUIROGA08>
2025-08-18 16:51:56 -0300

    landing nuevo

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+    <title>EMPRESA</title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="logo.jpeg" alt="logo">
+        </div>
+        <nav>
+            <div >
+                <ul>
+                    <a href="file:///C:/Users/PC/Documents/html/quorun.html"><li>Pagina principal</li></a>
+                    <a href="https://www.youtube.com/watch?v=7V3zyLm82_4"><li>Paquetes</li></a>
+                    <a href="https://w.app/Quorun"><li>Redes</li></a>
+                    <a href="#DONDEENCONTRARNOS"><li>Ayuda</li></a>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
