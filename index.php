2f695a9ca81eb8a9c298645eed04f3b8b7a82f37
VALENTINQUIROGA08 <VALENTINQUIROGA08>
2025-08-29 14:11:54 -0300

    estilo varios

--- index.php
+++ index.php
@@ -8,21 +8,12 @@
     <title>EMPRESA</title>
 </head>
 <body>
-    <header>
-        <div>
-            <img src="logo.jpeg" alt="logo">
-        </div>
-        <nav>
-            <div >
-                <ul>
-                    <a href="file:///C:/Users/PC/Documents/html/quorun.html"><li>Pagina principal</li></a>
-                    <a href="https://www.youtube.com/watch?v=7V3zyLm82_4"><li>Paquetes</li></a>
-                    <a href="https://w.app/Quorun"><li>Redes</li></a>
-                    <a href="#DONDEENCONTRARNOS"><li>Ayuda</li></a>
-                </ul>
-            </div>
-        </nav>
-    </header>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+    </nav>
 
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
