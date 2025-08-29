46b4c3134cbbd59b8e669893624e368b38707942
VALENTINQUIROGA08 <VALENTINQUIROGA08>
2025-08-29 14:12:09 -0300

    estilos varios2

--- index.php
+++ index.php
@@ -13,7 +13,19 @@
         <label for="check" class="checkbtn">
             <i class="fas fa-bars"></i>
         </label>
+        <a href="#" class="enlace">
+            <img src="logo.jpeg" alt="" class="logo">
+        </a>
+        <ul>
+            <li><a class="active "href="#">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">QSY</a></li>
+            <li><a href="#">OPT</a></li>
+            <li><a href="#">HOLA</a></li>
+
+        </ul>
     </nav>
+    <section></section>
 
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
