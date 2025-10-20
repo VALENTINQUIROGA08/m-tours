5fc951c7c0ef5930e243f0a5bc4973df5c96b4a9
VALENTINQUIROGA08 <VALENTINQUIROGA08>
2025-10-20 16:55:13 -0300

    qsy.2

---
+++ componentes/menu.php
@@ -0,0 +1,14 @@
+   <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <a href="#" class="enlace">
+            <img src="logo.png" alt="" class="logo">
+        </a>
+        <ul>
+            <li><a class="active" href="#">Página principal</a></li>
+            <li class="paquetes"><a href="#">Paquetes</a></li>
+            <li class="contacto"><a href="#">Contacto</a></li>
+        </ul>
+    </nav>
