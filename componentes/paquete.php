c4d4894afe085a8e7c35b075335e5d47c1dfbe21
VALENTINQUIROGA08 <VALENTINQUIROGA08>
2025-10-24 15:10:58 -0300

    cambio4

--- componentes/paquete.php
+++ componentes/paquete.php
@@ -5,14 +5,15 @@ echo encabezadoPaquete(titulo: "¡M-Tours - detalle del paquete!");
 
 $paqueteId = isset($_GET['id_paquete']) ? intval(value: $_GET['id_paquete']) : 0;
 
-if ($paqueteId !null && $paqueteId > 0) 
+if ($paqueteId !null && $paqueteId > 0) {
     require_once 'componentes/conexion.php';
 
     $paqueteId = $conexion->query("SELECT * 
     FROM paquetes 
     WHERE paquetes.id_paquete = $paqueteId AND estado='disponible' OR paquetes.estado = proximo ;")->fetch_assoc();
-
-if(!$paqueteId) {
-    echo "<div class='alert alert-danger'>paquete no encontrado o no disponible.</div>";
-    exit;   
-} else
+}
+    $servicios = $conexion->query("
+    SELECT *
+    FROM servicios JOIN paquete_servicio ON servicios.id_servicio = paquete_servicio.id_servicio
+    WHERE paquete_servicio.id_paquete = $paqueteId;");
+     
