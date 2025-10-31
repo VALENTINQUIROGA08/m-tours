23a08652816172f70d11afdbe35ecb8cca2c44f4
carosita <luqueselio5@gmail.com>
2025-10-31 13:41:18 -0300

    q

--- componentes/paquete.php
+++ componentes/paquete.php
@@ -5,7 +5,7 @@ echo encabezadoPaquete(titulo: "¡M-Tours - detalle del paquete!");
 
 $paqueteId = isset($_GET['id_paquete']) ? intval(value: $_GET['id_paquete']) : 0;
 
-if ($paqueteId !null && $paqueteId > 0) {
+if ($paqueteId !                          null && $paqueteId > 0) {
     require_once 'componentes/conexion.php';
 
     $paqueteId = $conexion->query("SELECT * 
