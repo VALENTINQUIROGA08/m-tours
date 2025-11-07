15fc04f526e718ea05cfdaf3d200b8f491bcec1b
josefinaprieto7 <josefinprie@gmail.com>
2025-11-07 15:06:53 -0300

    kokokok

--- pagina.php
+++ pagina.php
@@ -1,8 +1,23 @@
 <?php
 require_once 'componentes/conexion.php';
+require_once 'componentes/paquete.php';
 
-$paquetes = $conexion->query("SELECT * FROM paquetes WHERE estado='disponible';");
-
+function pagina($titulo): string
+{
+    return
+    '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="index.css">
+    <title> . $titulo . </title>
+</head>
+<body>
+';
+}
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +32,6 @@ $paquetes = $conexion->query("SELECT * FROM paquetes WHERE estado='disponible';"
 </head>
 <body>
    <?php require_once 'componentes/menu.php'; ?>
-   <?php require_once 'componentes/paquete.php'; ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
