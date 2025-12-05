bb72fae8161b2d136af551e91650abe983b5ae0c
VALENTINQUIROGA08 <VALENTINQUIROGA08>
2025-12-05 14:43:56 -0300

    gfyjfjy

--- login.php
+++ login.php
@@ -44,20 +44,27 @@ if ($_SERVER['REQUEST_METHOD'] && isset($_POST['ingresar'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <title>Document</title>
+        <link rel="stylesheet" href="index2.css">
+       <title>Document</title>
 </head>
 
 <body>
-    <form method="POST" action="login.php">
+    <div class="card" style="border-radius: 15px; background-color: rgba(0, 214, 230, 1);">
+        <h1>Iniciar Sesion</h1>
 
-        <?php require_once 'componentes/comp-form.php'; ?>
-        
-        <input type="submit" value="ingresar" name="ingresar" id="ingresar">
-    </form>
+            
+            <input type="submit" value="ingresar" name="ingresar" id="ingresar">
+        </form>
+    </div>
 
     <div>
         <p>¿NO TIENES USUARIO?registrate aqui: <a href="registro.php">aqui</a></p>
+
+
+        <form method="POST" action="login.php">
+        <?php require_once 'componentes/comp-form.php'; ?>
     </div>
+    
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
